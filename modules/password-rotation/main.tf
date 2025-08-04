9ce554bfc50fa3df4644923e7b85dec56af5ba2c
Syed Kazmi <35359630+muzammilkazmi86@users.noreply.github.com>
2025-08-04 14:37:57 -0500

    Update main.tf

--- modules/password-rotation/main.tf
+++ modules/password-rotation/main.tf
@@ -106,7 +106,7 @@ resource "aws_lambda_function" "password_rotation" {
   role            = aws_iam_role.lambda_rotation_role.arn
   handler         = "lambda_function.lambda_handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime         = "python3.9"
+  runtime         = "python3.12"
   timeout         = var.lambda_timeout
 
   # No VPC configuration - Lambda runs in AWS managed VPC
